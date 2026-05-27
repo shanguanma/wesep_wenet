7d9a1fcb59bdb9ed53a527f7f3fb8dcc567b6067
shanguanma <nanr9544@gmail.com>
2026-05-27 10:05:37 +0800

    add seperator causal exp on 4090d

--- examples/visual/voxceleb2mix/run_md.sh
+++ examples/visual/voxceleb2mix/run_md.sh
@@ -239,3 +239,69 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$num_gpus \
     --seed 42
 fi
 
+
+if [ ${stage} -le 117 ] && [ ${stop_stage} -ge 117 ]; then
+data_path_of_voxceleb2=/share/workspace/shared_datasets/speechdata/02_voxceleb2/01_voxceleb2_zk/mp4/train
+lrs3_root=/share/workspace/shared_datasets/speechdata/11_lrs3/01_lrs3_ljj/mp4/trainval
+exp_dir=/share/workspace3/maduo/exp/wesep_wenet/TSE_BSRNN_VISUAL_stage111_av_align_big_batch_lr_separator_causal_true
+num_gpus=1
+export TENSORBOARD_LOGGING_DIR=$exp_dir/logs
+export OMP_NUM_THREADS=8
+torchrun --standalone --nnodes=1 --nproc_per_node=$num_gpus \
+    wesep/bin/train_with_transformers_multi_data_online.py \
+    --model_config confs/tse_bsrnn_visual_model_v2.yaml \
+    --mp4_dir_of_voxceleb2 $data_path_of_voxceleb2 \
+    --use_voxceleb2 True \
+    --use_lrs3 True \
+    --use_chinese_lips False \
+    --lrs3_root $lrs3_root \
+    --chinese_lips_root "" \
+    --train_speaker_fraction 0.8 \
+    --output_dir $exp_dir \
+    --tse_model 'TSE_BSRNN_VISUAL' \
+    --loss_type 'SISDR_MRSTFT' \
+    --visual_frontend "muse" \
+    --separator_causal true \
+    --sample_num_per_epoch 60000 \
+    --sample_num_per_epoch_val 3000 \
+    --noise_lmdb_file /maduo/exp/wesep_wenet/datasets/voxceleb2mix/data/musan/lmdb \
+    --noise_prob 0.0 \
+    --online_mix_deterministic true \
+    --online_av_align true \
+    --num_speakers_max 2\
+    --num_speakers_distribution 0,1,0\
+    --max_steps 200000 \
+    --num_train_epochs 200000 \
+    --per_device_train_batch_size 6 \
+    --gradient_accumulation_steps 8 \
+    --dataloader_num_workers 2 \
+    --learning_rate 3.0e-4 \
+    --lr_scheduler_type cosine \
+    --warmup_steps 3000 \
+    --weight_decay 0.01 \
+    --max_grad_norm 5.0 \
+    --mrstft_weight 0.1 \
+    --mrstft_warmup_steps 5000 \
+    --cue_discrim_weight 0.001 \
+    --cue_discrim_warmup_steps 15000 \
+    --cue_discrim_temperature 8.0 \
+    --passthrough_penalty_weight 0.5 \
+    --passthrough_penalty_threshold 0.0 \
+    --eval_loss_type SISDR \
+    --eval_extra_stats true \
+    --eval_clearervoice_metrics true \
+    --eval_audio_sr 16000 \
+    --train_log_clearervoice_sisnri true \
+    --visual_cue_dropout 0.15 \
+    --use_ema false \
+    --ema_decay 0.999 \
+    --logging_steps 100 \
+    --save_steps 100 \
+    --save_total_limit 3 \
+    --save_strategy steps \
+    --eval_strategy steps \
+    --eval_steps 1000 \
+    --bf16 true \
+    --visual_max_frames 75
+fi
+
